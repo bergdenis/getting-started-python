b2b654e9e790ab9679c60345ee510764503ba9d2
Denis Berg <bergdenis@seznam.cz>
2023-06-21 00:47:58 +0200

    Выполнена первая домашняя работа

---
+++ google_should_find_selene.py
@@ -0,0 +1,7 @@
+from selene.support.shared import browser
+from selene import be, have, by
+
+browser.open('https://google.com')
+browser.element(by.css("button#L2AGLb")).click()
+browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in ...'))
