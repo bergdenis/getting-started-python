bd29150e3e854d95b2f8d0a2588080c412525e64
Denis Berg <bergdenis@seznam.cz>
2023-06-22 23:17:02 +0200

    Добавлен еще один тест на поиск, который проверяет что поиск не выдает результатов по запросу

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -7,3 +7,10 @@ def test_google():
     browser.element(by.css("button#L2AGLb")).click()
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in ...'))
+
+
+def test_no_results_in_google():
+    browser.open('https://google.com')
+    # browser.element(by.css("button#L2AGLb")).click()
+    browser.element('[name="q"]').should(be.blank).type('n.r.strukturalizovateln.mu').press_enter()
+    browser.element('[id="res"]').should(have.text(' - nebyl nalezen žádný odkaz.'))
